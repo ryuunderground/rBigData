85fd659b8a0bc22b537e63a35d08fb3cca4622da
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-05-31 02:58:21 +0900

    번식학 실습

--- noName.R
+++ noName.R
@@ -1,33 +1,12 @@
-gender <- c("m", "m","f","f","f","f")
-gender
-g_factor <- factor(gender)
-g_factor
-levels(g_factor)
-g_factor2 <- factor(gender, levels = c("m","f"), labels = c("Male", "Female"))
-g_factor2
-g_factor3 <- factor(gender, ordered = TRUE)
-g_factor3
-
-v1 <- 1:3
-v2 <- 4:6
-m1 <- rbind(v1, v2)
-m2 <- cbind(v1, v2)
-m1
-m2
-
-matrix(1:9, 3,3,byrow = TRUE)
-matrix(1:8, nrow = 4)
-matrix(5:15, ncol = 5)
-
-a1 <- array(1:10, dim = 10)
-a2 <- array(1:10, dim=c(2,5))
-a3 <- array(1:10, dim=c(3,3,4))
-a1
-a2
-a3
-
-d1 <- list(v2, m1)
-d1
-
-d2 <- list(a3, a2, v2, m2)
-d2
+AR <- 5
+F <- 8
+B <- 11
+D <- 33
+Pattern <- c("AR","F","B","D")
+Amount <- c(AR, F, B, D)
+df <- data.frame(Name, Amount)
+df
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data = df, mapping = aes(x=Pattern, y = Amount, fill = Pattern)) + geom_bar(stat = "identity", width = .4) + scale_x_discrete(limits =  c("AR","F","B","D"))
+Pattern
